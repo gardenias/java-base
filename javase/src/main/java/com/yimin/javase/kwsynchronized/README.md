74006295f14f0ab21adeb1ce0856f75485b59102
WuYimin <wym.missyou@gmail.com>
2015-05-25 23:58:25 +0800

    synchronized

---
+++ javase/src/main/java/com/yimin/javase/kwsynchronized/README.md
@@ -0,0 +1,4 @@
+## java keyword _`synchronized`_  
+
+1. **synchronized method**  
+2. **synchronized code block**
--- javase/src/test/java/kwsynchronized/AccountTest.java
+++ javase/src/test/java/kwsynchronized/AccountTest.java
@@ -1,6 +1,8 @@
 package kwsynchronized;
 
 import com.yimin.javase.kwsynchronized.Account;
+import com.yimin.javase.kwsynchronized.SynchronizedAccount;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -33,5 +35,32 @@ public class AccountTest {
         }
         if (acc.getBalance() == 0) System.out.println(acc);
         else System.err.println(acc);
+    }
+
+    @Test
+    public void synchronizedAccountInstanceLockTest() {
+        Thread[] threads = new Thread[THREAD_NUM];
+        final SynchronizedAccount acc = new SynchronizedAccount("John", 0);
+
+        for (int i = 0; i < THREAD_NUM; i++) {
+            threads[i] = new Thread(new Runnable() {
+                public void run() {
+                    acc.deposit(100l);
+                    acc.withdraw(100l);
+                }
+            });
+            threads[i].start();
+        }
+
+        for (int i = 0; i < THREAD_NUM; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+            }
+        }
+        if (acc.getBalance() == 0) System.out.println(acc);
+        else System.err.println(acc);
+
+        Assert.assertEquals("synchronized method",0,acc.getBalance(),0);
     }
 }
