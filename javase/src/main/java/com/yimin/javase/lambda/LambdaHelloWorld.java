894eff630e27c32118bef087a50cadf47533c002
WuYimin <wym.missyou@gmail.com>
2015-06-24 17:38:37 +0800

    java 8 Lambda 表达式使用方式测试

---
+++ javase/src/main/java/com/yimin/javase/lambda/LambdaHelloWorld.java
@@ -0,0 +1,16 @@
+package com.yimin.javase.lambda;
+
+/**
+ * java-libs
+ * Created by WuYimin on 2015/6/19.
+ */
+public class LambdaHelloWorld {
+    interface HelloWorld {
+        String hello(String name);
+    }
+
+    public static void main(String[] args) {
+        HelloWorld helloWorld = (String name) -> { return "Hello " + name; };
+        System.out.println(helloWorld.hello("Joe"));
+    }
+}
---
+++ javase/src/main/java/com/yimin/javase/lambda/LambdaInitialization.java
@@ -0,0 +1,31 @@
+package com.yimin.javase.lambda;
+
+import java.util.Arrays;
+import java.util.concurrent.Callable;
+
+/**
+ * java-libs
+ * Created by WuYimin on 2015/6/19.
+ */
+public class LambdaInitialization {
+    public static void main(String args[]) throws Exception {
+        Callable[] animals = new Callable[]{() -> "Lion", () -> "Crocodile"};
+
+        System.out.println(animals.length);
+        Arrays.asList(animals).forEach(c -> {
+            try {
+                System.out.println(c.call());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        Callable animals2 = (Callable) () -> {
+            System.out.println("method body");
+            return "Hello";
+        };
+
+        System.out.println(animals.length);
+        System.out.println(animals2.call());
+    }
+}
